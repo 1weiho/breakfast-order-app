691faa32e8f3359776e6c823df8e95b76bf6dd24
Ho Yi Wei <75478661+davidho0403@users.noreply.github.com>
2023-04-25 00:55:12 +0800

    Implement product card layout (#48)

---
+++ app/src/main/java/com/fcu/breakfast_order_app/ProductCard.java
@@ -0,0 +1,50 @@
+package com.fcu.breakfast_order_app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ProductCard extends LinearLayout {
+    private ImageView productImage;
+    private TextView productName;
+    private TextView productPrice;
+
+    public ProductCard(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ProductCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ProductCard(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.product_card, this);
+
+        productImage = findViewById(R.id.productImage);
+        productName = findViewById(R.id.productName);
+        productPrice = findViewById(R.id.productPrice);
+    }
+
+    public void setProductImage(int resId) {
+        productImage.setImageResource(resId);
+    }
+
+    public void setProductName(String name) {
+        productName.setText(name);
+    }
+
+    public void setProductPrice(String price) {
+        productPrice.setText(price);
+    }
+}
