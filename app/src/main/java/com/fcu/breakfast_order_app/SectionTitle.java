9be5e1f7c8c98fd8a5c4ed407f6b256b6bf1a678
TTigger <102512862+TTigger@users.noreply.github.com>
2023-04-25 20:13:45 +0800

    fix SectionTitle.java

--- app/src/main/java/com/fcu/breakfast_order_app/SectionTitle.java
+++ app/src/main/java/com/fcu/breakfast_order_app/SectionTitle.java
@@ -3,7 +3,6 @@ package com.fcu.breakfast_order_app;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
