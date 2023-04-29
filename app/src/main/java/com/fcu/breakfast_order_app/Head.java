40fe919328063bfa0b73d94f41c00c771ffdc58c
Ho Yi Wei <75478661+davidho0403@users.noreply.github.com>
2023-04-29 22:21:19 +0800

    feat: head component layout (#60)

---
+++ app/src/main/java/com/fcu/breakfast_order_app/Head.java
@@ -0,0 +1,39 @@
+package com.fcu.breakfast_order_app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Head extends LinearLayout {
+
+    private TextView headTitle;
+
+    public Head(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public Head(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public Head(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.head, this);
+
+        headTitle = findViewById(R.id.headTitle);
+    }
+
+    public void setHeadTitle(String title) {
+        headTitle.setText(title);
+    }
+}
