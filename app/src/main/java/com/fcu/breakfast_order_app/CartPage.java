2416c0f6c175e851e784e5e96a2905b875bacf67
TTigger <102512862+TTigger@users.noreply.github.com>
2023-05-08 15:57:15 +0800

    Implement CartPage

---
+++ app/src/main/java/com/fcu/breakfast_order_app/CartPage.java
@@ -0,0 +1,14 @@
+package com.fcu.breakfast_order_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartPage extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.cart_page);
+  }
+}
