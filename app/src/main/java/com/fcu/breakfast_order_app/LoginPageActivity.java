c2b1317a90103ed8f8d45954c5c4143ddbbdfa99
TTigger <102512862+TTigger@users.noreply.github.com>
2023-05-22 15:05:40 +0800

    update LoginPage

--- app/src/main/java/com/fcu/breakfast_order_app/LoginPageActivity.java
+++ app/src/main/java/com/fcu/breakfast_order_app/LoginPageActivity.java
@@ -4,14 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginPage extends AppCompatActivity {
+public class LoginPageActivity extends AppCompatActivity {
 
     private Head head;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.activity_login_page);
 
         head = findViewById(R.id.headtitle);
         head.setHeadTitle("登入");
