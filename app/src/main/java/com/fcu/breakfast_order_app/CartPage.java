169bcb9e7a09e17e09740530e7144cc0a14b42b7
TTigger <102512862+TTigger@users.noreply.github.com>
2023-05-22 14:14:57 +0800

    update

--- app/src/main/java/com/fcu/breakfast_order_app/CartPage.java
+++ app/src/main/java/com/fcu/breakfast_order_app/CartPage.java
@@ -11,6 +11,7 @@ public class CartPage extends AppCompatActivity {
   private CartItem cartItem2;
   private CartItem cartItem3;
   private CartItem cartItem4;
+  private BackButton cart_back_button;
 
 
   @Override
@@ -23,23 +24,27 @@ public class CartPage extends AppCompatActivity {
     cartItem2 = findViewById(R.id.cartItem2);
     cartItem3 = findViewById(R.id.cartItem3);
     cartItem4 = findViewById(R.id.cartItem4);
+    cart_back_button = findViewById(R.id.cart_back_button);
 
     cart_headTitle.setHeadTitle("購物車");
 
     cartItem1.setCartName("紫米時蔬嫩雞飯");
     cartItem1.setCartPrice("NT 100");
-    cartItem1.setCartImage(R.drawable.food_img);
+    cartItem1.setCartImage(R.drawable.food_img1);
 
     cartItem2.setCartName("黑狗套餐");
     cartItem2.setCartPrice("NT 110");
-    cartItem2.setCartImage(R.drawable.food_img);
+    cartItem2.setCartImage(R.drawable.food_img1);
 
     cartItem3.setCartName("熱狗香蕉套餐");
     cartItem3.setCartPrice("NT 120");
-    cartItem3.setCartImage(R.drawable.food_img);
+    cartItem3.setCartImage(R.drawable.food_img1);
 
     cartItem4.setCartName("國軍敬禮套餐");
     cartItem4.setCartPrice("NT 130");
-    cartItem4.setCartImage(R.drawable.food_img);
+    cartItem4.setCartImage(R.drawable.food_img1);
+
+
+    cart_back_button.setLabel("繼續點餐");
   }
 }
