e459ae5fcc2c859b6775fe4fd8fe0a2c091db170
Ho Yi Wei <75478661+davidho0403@users.noreply.github.com>
2023-05-29 03:48:16 +0800

    Implement the authentication feature and database (#75)
    * feat: implement authentication and DB setup
    
    * feat: save the login session

--- app/src/main/java/com/fcu/breakfast_order_app/RegisterPageActivity.java
+++ app/src/main/java/com/fcu/breakfast_order_app/RegisterPageActivity.java
@@ -2,11 +2,19 @@ package com.fcu.breakfast_order_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterPageActivity extends AppCompatActivity {
 
     private Head head;
+    private EditText phone;
+    private EditText password;
+    private Button registerButton;
+    private DatabaseHandler databaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,22 @@ public class RegisterPageActivity extends AppCompatActivity {
 
         head = findViewById(R.id.headtitle);
         head.setHeadTitle("註冊");
+
+        phone = findViewById(R.id.editTextPhone);
+        password = findViewById(R.id.editTextPassword);
+        registerButton = findViewById(R.id.submitButton);
+        registerButton.setOnClickListener(v -> {
+            databaseHandler = new DatabaseHandler(this);
+            databaseHandler.open();
+            if (databaseHandler.register(phone.getText().toString(), password.getText().toString())) {
+                Toast.makeText(this, "註冊成功", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(RegisterPageActivity.this, LoginPageActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "註冊失敗", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
